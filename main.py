7f75eca158a1b6ca75536a5790127d1975c3fbd0
DaffaDazuqo <62296831+DaffaDazuqo@users.noreply.github.com>
2022-11-07 10:14:31 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+
+# Soal 1: Hello World to Python
+say = (
+        "Hello World to Pyhton"
+        )
+
+print (say)
+
+# Soal 2: Aritmatika di Python
+A1 = 10
+A2 = 20
+A3 = 30
+A4 = 40
+A5 = 50
+
+print (
+        "Pertambahan :", A1 + A5, A3 + A2
+        )
+print (
+        "Pengurangan :", A2 - A1, A5 - A3
+        )
+print (
+        "Perkalian :", A1 * A5, A3 * A2
+        )
+print (
+        "Pembagian :", A1 / A5, A3 / A2
+        )
+
+# Quiz 3: Assign Variable dan Tipe Data Integer, Float
+a = 5
+b = 25
+
+print (
+        "Integer :", b//a
+        )
+print ("Float :", a/b)
+
+# Soal 4: Tipe Data
+usia = "16 Tahun"
+Daffa = "Usiaku"
+
+print (
+    "Tipe Data : "
+    '{} {}'.format(Daffa, usia)
+)
+
+
